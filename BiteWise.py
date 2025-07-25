b53da2860c67b4bebf921ce17c003465b7bc0df8
Lukas Schaub <lukas_schaub@gmx.ch>
2025-07-25 23:27:12 +0200

    modified requirements to get BeautifulSoup, working on requesting function within BiteWise.py

--- BiteWise.py
+++ BiteWise.py
@@ -18,8 +18,8 @@ def main():
     )
 
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--test" ,type=str, help="A Demo for our friend Boots the Bear")
-    group.add_argument("-t" ,type=str, help="A Demo for our friend Boots the Bear")
+    group.add_argument("--poc" ,type=str, help="A Demo for our friend Boots the Bear")
+    group.add_argument("-p" ,type=str, help="A Demo for our friend Boots the Bear")
     group.add_argument("--url" ,type=str, help="URL to a recipie")
     group.add_argument("-u" ,type=str, help="URL to a recipie")
     group.add_argument("--name", type=str, help="Name of a recipe to search for")
@@ -27,18 +27,54 @@ def main():
 
     args = parser.parse_args()
 
-    if args.test or args.t:
+    if args.poc or args.p:
         example()
     elif args.url or args.u:
         url_recipe(args.url)
     elif args.name or args.n:
         name_recipe(args.name)
-    
 
+
+"""
+Before Refactoring Area
+"""
+
+
+
+import requests 
+from bs4 import BeautifulSoup
+
+def url_parser(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+    except:
+        ConnectionError("Not able to fetch any data")
+        exit(1)
+
+    title = soup.find("h1").text
+
+    ingredients = []
+    ing_list = soup.find_all("li", class_="ingredient")
+    for li in ing_list:
+        ingredients.append(li.text)
+
+    img_tag = soup.find("img")
+    if img_tag:
+        img_url = img_tag.get("src")
+
+        print(ingredients,title)
+        return img_url
+    
+    else:
+        print(ingredients,title)
+        return "123.com"
 
 def test():
-    img_url = "TESTURL.CX"
-    provide_image("TESTURL.CX")
+    url = "https://hot-thai-kitchen.com/laab-gai/"
+    img_url = url_parser(url)
+    
+    provide_image(img_url)
     print("NUTRITION INFO")
     print("Protionsize")
     print("Cooking Instructions")
@@ -46,10 +82,13 @@ def test():
 
 
 
+"""
+End of Before Refactoring Area
+"""
 
 
 if __name__ == "__main__":
+    """
     main()
     """
     test()
-    """
--- utils/image_provider.py
+++ utils/image_provider.py
@@ -1,6 +1,5 @@
 from term_image.image import from_url, Size
 
-
 def provide_image(url):
     try:
         image = from_url(url)
@@ -8,7 +7,8 @@ def provide_image(url):
         print("Cannot find an image at provided URL using a placeholder")
         image = from_url("https://www.boot.dev/_nuxt/baked-salmon.QSU05kuT.webp",width=Size.FIT_TO_WIDTH)
     try:
-        image.draw()
+        image.draw() 
+        #Looks kind of funny zsh terminal.
     except:
         print("\n\nYou are trying to eat more than your terminal can chew.\nChange your terminal size and try again.\n\n")
         
